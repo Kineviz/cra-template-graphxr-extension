eff0baf78b890158ff2be5c5b65ca90310249a62
SeanLi <sean@kineviz.com>
2020-11-10 23:24:14 +0800

    improve the vscode tips

--- template/src/App.jsx
+++ template/src/App.jsx
@@ -3,13 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="app-link"
           href="https://ant.design/components/overview/"
           target="_blank"
           rel="noopener noreferrer"
